915e5d6589e26e5cbea5a0a8d60c9af3f65bc529
Baptiste Morisse <bmorisse.pro@proton.me>
2024-05-29 16:17:12 +0200

    Upgrade test with cosine and multiheaded possibilites for Vector Quantizer

--- tests/test_vector_quantize.py
+++ tests/test_vector_quantize.py
@@ -20,3 +20,42 @@ class TestVectorQuantizer:
 
         assert x.shape == quantized.shape
         assert indices.shape == (1, 1024)
+
+class TestVectorQuantizerCosine:
+
+    vq = VectorQuantize(
+        dim = 256,
+        codebook_size = 512,     # codebook size
+        use_cosine_sim=True
+    )
+
+    def test_init(self):
+        assert self.vq
+
+    def test_forward(self):
+        x = torch.randn(1, 1024, 256)
+        quantized, indices, _ = self.vq(x)
+
+        assert x.shape == quantized.shape
+        assert indices.shape == (1, 1024)
+
+class TestVectorQuantizerMultihead:
+
+    vq = VectorQuantize(
+        dim = 256,
+        codebook_dim = 32,                  # a number of papers have shown smaller codebook dimension to be acceptable
+        heads = 8,                          # number of heads to vector quantize, codebook shared across all heads
+        separate_codebook_per_head = True,  # whether to have a separate codebook per head. False would mean 1 shared codebook
+        codebook_size = 8196,
+        accept_image_fmap = True
+    )
+
+    def test_init(self):
+        assert self.vq
+
+    def test_forward(self):
+        img_fmap = torch.randn(1, 256, 32, 32)
+        quantized, indices, _ = self.vq(img_fmap)
+
+        assert img_fmap.shape == quantized.shape
+        assert indices.shape == (1, 32, 32, 8)
