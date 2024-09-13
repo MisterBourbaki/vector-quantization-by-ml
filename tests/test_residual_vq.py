a61abff02682adf82713f22d265430be7a038750
Baptiste Morisse <bmorisse.pro@proton.me>
2024-09-13 18:02:06 +0200

    Update tests for ResidualVQ accordingly to the last change of API, introducing codebook_params.

--- tests/test_residual_vq.py
+++ tests/test_residual_vq.py
@@ -1,16 +1,20 @@
 import torch
 
+from vector_quantize_pytorch.codebooks import CodebookParams, KmeansParameters
 from vector_quantize_pytorch.residual_vq import GroupedResidualVQ, ResidualVQ
 
 
 class TestResidualVQ:
     dim = 4
     num_quantizers = 3
+    codebook_size = 2**5
+    codebook_params = CodebookParams(dim=dim, codebook_size=codebook_size)
 
     quantizer = ResidualVQ(
         dim=dim,
         num_quantizers=num_quantizers,  # specify number of quantizers
         codebook_size=2**5,  # codebook size
+        codebook_params=codebook_params,
     )
 
     def test_init(self):
@@ -31,6 +35,8 @@ class TestResidualVQ:
 class TestResidualVQ2:
     dim = 4
     num_quantizers = 3
+    codebook_size = 2**5
+    codebook_params = CodebookParams(dim=dim, codebook_size=codebook_size)
 
     quantizer = ResidualVQ(
         dim=dim,
@@ -39,6 +45,7 @@ class TestResidualVQ2:
         stochastic_sample_codes=True,
         sample_codebook_temp=0.1,  # temperature for stochastically sampling codes, 0 would be equivalent to non-stochastic
         shared_codebook=True,  # whether to share the codebooks for all quantizers or not
+        codebook_params=codebook_params,
     )
 
     def test_init(self):
@@ -59,12 +66,21 @@ class TestResidualVQ2:
 class TestResidualVQ3:
     dim = 4
     num_quantizers = 3
+    codebook_size = 2**5
+    kmeans_params = KmeansParameters()
+    codebook_params = CodebookParams(
+        dim=dim,
+        codebook_size=codebook_size,
+        initialization_by_kmeans=True,
+        kmeans_params=kmeans_params,
+    )
 
     quantizer = ResidualVQ(
         dim=dim,
         num_quantizers=num_quantizers,
         codebook_size=2**5,
-        initialization_by_kmeans=True,  # set to True
+        codebook_params=codebook_params,
+        # initialization_by_kmeans=True,  # set to True
         # kmeans_iters=10,  # number of kmeans iterations to calculate the centroids for the codebook on init
     )
 
@@ -87,12 +103,15 @@ class TestGroupedResidualVQ:
     dim = 4
     num_quantizers = 3
     groups = 2
+    codebook_size = 2**5
+    codebook_params = CodebookParams(dim=dim, codebook_size=codebook_size)
 
     quantizer = GroupedResidualVQ(
         dim=dim,
         num_quantizers=num_quantizers,  # specify number of quantizers
         groups=groups,
         codebook_size=2**5,  # codebook size
+        codebook_params=codebook_params,
     )
 
     def test_init(self):
@@ -111,11 +130,23 @@ class TestGroupedResidualVQ:
 
 
 def test_residual_vq3():
+    dim = 4
+    # num_quantizers = 3
+    codebook_size = 2**5
+    kmeans_params = KmeansParameters()
+    codebook_params = CodebookParams(
+        dim=dim,
+        codebook_size=codebook_size,
+        initialization_by_kmeans=True,
+        kmeans_params=kmeans_params,
+    )
+
     quantizer = ResidualVQ(
         dim=4,
         codebook_size=2**5,
         num_quantizers=4,
-        initialization_by_kmeans=True,  # set to True
+        codebook_params=codebook_params,
+        # initialization_by_kmeans=True,  # set to True
         # kmeans_iters=10,  # number of kmeans iterations to calculate the centroids for the codebook on init
     )
 
